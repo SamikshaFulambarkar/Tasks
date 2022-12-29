6a38984a537711fb54fd6617869e047a67f2306e
Samiksha Fulambarkar <samikshafulambarkar99@gmail.com>
2022-12-29 19:53:25 +0530

    mycomit

--- myfirstproject/CollectionPrograms/StructDemo.cs
+++ myfirstproject/CollectionPrograms/StructDemo.cs
@@ -23,4 +23,69 @@ namespace myfirstproject.CollectionDemo
             Console.ReadKey();
         }
     }
+    struct Mystruct2
+    {
+        public int x;
+        public int y;
+
+        public Mystruct2(int x, int y )
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    class StructDemo2
+    {
+        static void Main(string[] args)
+        {
+            Mystruct2 s = new Mystruct2(100, 200);
+            Console.WriteLine(s.x);
+            Console.WriteLine(s.y);
+        }
+    }
+    struct Mystruct3
+    {
+        int x;
+        int y;
+
+        public int X
+        {
+            set;
+            get;
+        }
+        public int Y
+        {
+            set;
+            get;
+        }
+    }
+    class StructDemo3
+    {
+        static void Main(string[] args)
+        {
+            Mystruct3 s = new Mystruct3();
+            s.X = 90;
+            s.Y = 83;
+            Console.WriteLine(s.X);
+            Console.WriteLine(s.Y);
+        }
+    }
+    struct Mystruct4
+    {
+        int x;
+        int y;
+
+        public void MyData()
+        {
+            Console.WriteLine("This is My Method....................");
+        }
+    }
+    class StructDemo4
+    {
+        static void Main(string[] args)
+        {
+            Mystruct4 s = new Mystruct4();
+            s.MyData();
+        }
+    }
 }
